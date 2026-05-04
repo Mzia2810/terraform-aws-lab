1637f48a0ee861d7272450f0fd7a2e9e42454a17
Muhammad Zia <muhammadzia@Muhammads-MacBook-Pro.local>
2026-05-05 03:38:17 +0400

    creating EC2 in public subnet and private subnet with security group

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -23,11 +23,12 @@ module "vpc" {
 module "ec2" {
   source = "../../modules/ec2"
 
-  vpc_id            = module.vpc.vpc_id
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+
   public_subnet_id  = module.vpc.public_subnet_ids[0]
   private_subnet_id = module.vpc.private_subnet_ids[0]
 
   public_sg_id  = module.vpc.public_sg_id
   private_sg_id = module.vpc.private_sg_id
-
 }
