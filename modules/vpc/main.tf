c1c74874ab4496ddc7925468e10ab16fce1caca1
Muhammad Zia <muhammadzia@Muhammads-MacBook-Pro.local>
2026-05-05 02:37:20 +0400

    creating private subnet association for routing

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -94,3 +94,11 @@ resource "aws_route_table" "private" {
     Name = "private-rt"
   }
 }
+
+
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
