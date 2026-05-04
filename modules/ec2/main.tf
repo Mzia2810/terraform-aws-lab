fde6b39a81db2bdfc58e962dd6b2c1f9c906ecd9
Muhammad Zia <muhammadzia@Muhammads-MacBook-Pro.local>
2026-05-05 03:44:36 +0400

    creating EC2 in public subnet and private subnet with security group

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "public_ec2" {
   instance_type = var.instance_type
 
   subnet_id              = var.public_subnet_id
-  vpc_security_group_ids = [var.public_sg_id]
+  
 
   associate_public_ip_address = true
 
@@ -20,7 +20,7 @@ resource "aws_instance" "private_ec2" {
   instance_type = var.instance_type
 
   subnet_id              = var.private_subnet_id
-  vpc_security_group_ids = [var.private_sg_id]
+ 
 
   tags = {
     Name = "app-server"
