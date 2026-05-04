e839fa5ac23abc27ef6d3c509c0241e410e0664f
Muhammad Zia <muhammadzia@Muhammads-MacBook-Pro.local>
2026-05-05 02:08:47 +0400

    creating subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,29 @@ resource "aws_vpc" "main" {
     Name = var.vpc_name
   }
 }
+
+resource "aws_subnet" "public" {
+  count = 2
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+
+
+resource "aws_subnet" "private" {
+  count = 2
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -5,3 +5,11 @@ variable "vpc_cidr" {
 variable "vpc_name" {
   type = string
 }
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
