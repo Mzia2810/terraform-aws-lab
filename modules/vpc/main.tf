143bc42726f7832a1c73a55f87a98ee7d835bfbd
Muhammad Zia <muhammadzia@Muhammads-MacBook-Pro.local>
2026-05-05 02:14:45 +0400

    creating IGW

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -31,3 +31,12 @@ resource "aws_subnet" "private" {
     Name = "private-subnet-${count.index + 1}"
   }
 }
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
