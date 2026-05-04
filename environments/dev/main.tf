d6c81754a9f6ec1300dd74be44d17eca26f15fc7
Muhammad Zia <muhammadzia@Muhammads-MacBook-Pro.local>
2026-05-05 01:59:58 +0400

    Public and Private subnet creating

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,4 +3,17 @@ module "vpc" {
 
   vpc_cidr = "10.0.0.0/16"
   vpc_name = "dev-vpc"
+
+
+
+  public_subnet_cidrs  = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnet_cidrs = [
+    "10.0.3.0/24",
+    "10.0.4.0/24"
+  ]
+
 }
