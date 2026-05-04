4f2e57856dea3daa6f6f54d03349cdd97e3dfbed
Muhammad Zia <muhammadzia@Muhammads-MacBook-Pro.local>
2026-05-05 02:29:30 +0400

    Elastic Ip and Nat gate creating

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -64,3 +64,18 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
